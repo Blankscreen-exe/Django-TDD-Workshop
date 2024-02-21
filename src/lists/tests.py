18f95e502a99ac12ccd46377c8d7b95f2518a589
Muhammad Hammad Hassan <hammad.hassan@siliconplex.com>
2024-02-21 18:28:33 +0500

    initial tests

--- src/lists/tests.py
+++ src/lists/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from django.urls import resolve
+from lists.views import homepage
 
-# Create your tests here.
+class HomePageTest(TestCase):
+
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve("/")
+        self.assertEqual(found.func, home_page)
+
+    def dummy_test(self):
+        self.assertTrue(True)
