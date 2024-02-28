7934b8566a64d343294d73d1f96507c39fbd5c84
Muhammad Hammad Hassan <51869983+Blankscreen-exe@users.noreply.github.com>
2024-02-28 17:09:54 +0500

    add view

--- src/lists/views.py
+++ src/lists/views.py
@@ -1,3 +1,9 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-homepage = None
+
+def home_page(request):
+    html = """
+        <html><title>To-Do lists</title></html>
+    """
+    return HttpResponse(html)
