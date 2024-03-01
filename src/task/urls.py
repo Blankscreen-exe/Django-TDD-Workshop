9f4acf0b007255b6fd0acae0e3f48f6605a606f3
Muhammad Hammad Hassan <51869983+Blankscreen-exe@users.noreply.github.com>
2024-03-01 13:21:17 +0500

    fixed a typo

--- src/task/urls.py
+++ src/task/urls.py
@@ -3,6 +3,6 @@ from .views import *
 
 urlpatterns = [
     path("", index.as_view(), name='task-home'),
-    path("<int:task_id>/", detail.as_view(), name='task-details')
+    path("<int:task_id>/", detail.as_view(), name='task-details'),
     path("add/", addTask.as_view(), name='task-add')
 ]
